c8d590a78a8948ca0647426ef8c6d0bf5bd7f234
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-03-27 21:30:31 +0100

    added inputs counter

--- src/luca/Zaehler.java
+++ src/luca/Zaehler.java
@@ -7,6 +7,7 @@ public class Zaehler {
 		
 		int input = 0;
 		int summe = 0;
+		int inputs = 0;
 		
 		Scanner in = new Scanner(System.in);
 		
@@ -14,16 +15,18 @@ public class Zaehler {
 			System.out.println("Geben Sie eine Zahl ein: ");
 			input = in.nextInt();
 			
-			if (input % 2 == 0) {
-				summe = summe + input;
+			if (input % 2 == 0) {	//Überprüfung ob Rest 0 ist beim Teilen durch 2
+				summe = summe + input; 
 				System.out.println("Gerade, Zahl wurde addiert!");
 				System.out.println("Aktueller Stand: " + summe);
 			} else {
 				System.out.println("Zahl ungerade.");
 			}
+			inputs++;
 		}
 		System.out.println("Programm beendet!");
-		System.out.println("Gesammtsumme lautet:" + summe);
+		System.out.println("Eingaben: " + inputs);
+		System.out.println("Gesammtsumme lautet: " + summe);
 
 	}
 
