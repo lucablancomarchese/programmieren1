deeeb847666a3aa8fa611d44a585dc7f66125694
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-03-29 23:23:50 +0100

    aufgabe 1 done

--- src/luca/FahrenheitUmrechner.java
+++ src/luca/FahrenheitUmrechner.java
@@ -7,7 +7,7 @@ public class FahrenheitUmrechner {
 	public static void main(String[] args) {
 		
 				//Variablen deklaration:
-				double epsilon = 1.005;
+				double epsilon = 0.005;
 				double c = 0.0, f;
 				
 				// Variable zum einlesen:
@@ -15,17 +15,17 @@ public class FahrenheitUmrechner {
 				
 				do {
 					System.out.println("Programm endet durch Eingabe von 0");
-					System.out.println("Eingabe der Fahrenheit-Temperatur:");
+					System.out.println("Eingabe der Fahrenheit Temperatur:");
 					//Eingabe wird eingelesen und in Varible gemacht
-					c = in.nextDouble();
+					f = in.nextDouble();
 					
 					//Umrechnen in Fahrenheit
-					f = c* 9.0/5.0 + 32;
+					c = (f - 32) * 5/9;
 					
 					//Die Zahl in Fahrenheit ausgeben
-					System.out.println("... in Celsius " + f + " Grad Celsius \n");
+					System.out.println("... in Celsius " + c + " Grad Celsius \n");
 					
-				} while (Math.abs(c) > epsilon); //Math abs um aus einer Zahl einen Betrag zu machen:
+				} while (Math.abs(f - 1) > epsilon); //Math abs um aus einer Zahl einen Betrag zu machen:
 				
 				System.out.println("Programm beendet!");
 	}
