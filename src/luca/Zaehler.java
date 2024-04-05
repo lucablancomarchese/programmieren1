7320c09bb2f878245165fe1fbc42d963d977bcfd
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-04-05 10:45:56 +0200

    finished Zahler program and Fahrenheit programm

--- src/luca/Zaehler.java
+++ src/luca/Zaehler.java
@@ -1,31 +1,36 @@
 package luca;
+
 import java.util.Scanner;
 
 public class Zaehler {
-
+	
+	//Programm, dass ganzzahlige Benutzereingaben summiert, 
+	//wenn sie gerade sind und die Gesamtsumme und die Eingaben ausgibt.
+	
 	public static void main(String[] args) {
 		
-		int input = 0;
+		
+		int input;
 		int inputs = 0;
 		int sum = 0;
 		
 		Scanner in = new Scanner(System.in);
 		
-		while(input >= 0) {
+		do {
 			System.out.println("Geben Sie eine Zahl ein: ");
 			input = in.nextInt();
 			
-			if (input % 2 == 0 && input >= 0) {	//Überprüfung ob Rest 0 ist beim Teilen durch 2
-				sum = sum + input; 
-				System.out.println("Gerade, Zahl wurde addiert!");
-				System.out.println("Aktueller Stand: " + sum);
+			if(input >= 0) {
 				inputs++;
-			} else if(input >= 0){
-				System.out.println("Zahl ungerade.");
-				inputs++;
+				if(input % 2 == 0) {
+					sum = sum + input;
+					System.out.println("Aktueller Stand: " + sum);
+					
+				}
 			}
 			
-		}
+		} while( input >= 0);
+		
 		System.out.println("Programm beendet!");
 		System.out.println("Eingaben: " + inputs);
 		System.out.println("Gesammtsumme lautet: " + sum);
