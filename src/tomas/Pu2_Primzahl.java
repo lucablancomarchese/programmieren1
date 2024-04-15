791d4465e573d25540ebe7a4f5c902d48e9ac820
Tomas Parada <tomasparada123@gmail.com>
2024-04-15 10:37:21 +0200

    aufgabe1 PU2

---
+++ src/tomas/Pu2_Primzahl.java
@@ -0,0 +1,47 @@
+package tomas;
+
+import java.util.Scanner;
+
+public class Pu2_Primzahl {
+	public static void main(String[] args) {
+			
+			int input;
+			
+		
+			Scanner in = new Scanner(System.in);
+			System.out.println("Geben Sie eine Nummer ein: ");
+			
+				
+			input = in.nextInt();
+			
+			// First loop breaks down inputed number into a range excluding 1 (5 = 5,4,3,2)
+			
+			for(int i = 2; i <= input; i++) {
+				
+				boolean isPrime = true;
+				
+			// Checks if broken down numbers are divisible by 2.
+			// If divisible, then the number isn't prime.
+				
+				for(int j = 2; j <= i/2; j++) {
+					
+					if(i % j == 0) {
+						isPrime = false;
+						break;
+					}
+				}
+				
+			// If not divisible then number is prime and gets printed in the console.
+				
+				if(isPrime) {
+					System.out.println(i + " ist eine Primzahl");
+				}
+			}
+				
+				
+		}
+	}
+		
+	
+
+	
