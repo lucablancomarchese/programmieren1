0d919e68a19a68b2911f691d9a9ac9b1b38315fc
Tomas Parada <tomasparada123@gmail.com>
2024-04-17 09:11:50 +0200

    aufgabe 2 completion

--- src/tomas/PU2/pu2_alphabet.java
+++ src/tomas/PU2/pu2_alphabet.java
@@ -1,5 +1,58 @@
 package tomas.PU2;
 
-public class pu2_alphabet {
+import java.util.Scanner;
 
+public class pu2_alphabet {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your sentence: ");
+		
+		String input = sc.nextLine();
+		String inputString = String.valueOf(input);
+		String processedString = " ";
+		
+		
+		// First loop breaks down string into individual characters
+		
+		for (int i = 0; i < inputString.length(); i++) {
+			
+			char c = inputString.charAt(i);
+			
+		// Checks if elements of the string are letters, if so then it replaces every letter with the next one in the alphabet using ASCII.	
+		// Also checks for Upper case in order to keep upper-lower case parity in the transformation.
+			
+			if(Character.isLetter(c)) {
+				
+				// Check for lowercase letters.
+				if(c >= 'a' && c <= 'z') { 
+					
+					c = (char) (c + 1); 
+					
+				// Wrap around for lowercase 'z'.
+				} else if (c == 'z') { 
+					
+					c = 'a';
+					
+				// Check for uppercase letters.	
+				} else if (c >= 'A' || c < 'Z') { 
+					
+					c = (char) (c + 1);
+					
+				// Wrap around (z -> a) for uppercase 'Z'.
+				} else if (c == 'Z') { 
+					
+					c = 'A';
+				}
+				// Concatenate 'c' (Broken down input string) characters into one String.
+				processedString += c;
+			}			
+		}
+		
+		System.out.println("Input: " + input);
+		System.out.println("Output: " + processedString);
+		
+		sc.close();
+	}
 }
