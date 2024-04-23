428b312d537a4e205d9bbc3f4286db020bf86cef
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-04-23 11:29:58 +0200

    .

--- src/luca/PU2/RoemischeZahlen.java
+++ src/luca/PU2/RoemischeZahlen.java
@@ -1,17 +1,44 @@
 package luca.PU2;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class RoemischeZahlen {
 	
 	/*
 	 * Programm, dass eine eingegbene Ganzzahl,
 	 * in eine römische Zahl verändert.
-	 * */
+	*/
 	
 	final static int I = 1, X = 10, C = 100, M = 1000;
 	final static int V = 5, L = 50, D = 500;
 	
 	
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Gebe deine Zahl ein: ");
+		
+		int input = in.nextInt();
+		
+		int[] numbers;
+		int summe = 0;
+		
+		if(input > 0) {
+			
+			while(input > 0) {
+				
+			}
+		
+			
+			
+			
+		} else {
+			System.out.println("Bitte gebe eine Zahl, die Größer 0 ist ein!");
+		}
+		
+		
 		
 	}
 
