cc9aa43b61ebd43a6a5969445873135171a51407
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-04-23 19:36:56 +0200

    added loop to check user input

--- src/luca/PU2/Eratosthenes.java
+++ src/luca/PU2/Eratosthenes.java
@@ -15,43 +15,67 @@ public class Eratosthenes {
 		
 		Scanner in = new Scanner(System.in);
 		
-		System.out.print("Gebe dein Maximum ein: ");
-		
-		int input = in.nextInt() + 1;
-		
-		boolean[] numbers = new boolean[input];
 		
 		
-		// 0 und 1 auf true setzen, da sie sowieso keine Primzahlen sind
-		numbers[0] = true;
-		numbers[1] = true;
-	
 		
-		// Schleife um Primzahlen zu sortieren.
-		for (int i = 2; i < input - i; i++) {
+		
+		int input;
+		
+		do {
+			 System.out.print("Gebe dein Maximum ein: ");
+			 input = in.nextInt();
+			 if (input < 2) {
+				 System.out.println("Bitte gebe ein Maximum, dass größer als 2 ist ein!");
+			} else {
+				input++;
+			}
+		} while(input < 2);
+		
+		
+		
+		
+			boolean[] numbers = new boolean[input];
 			
-			for (int j = i + 1; j < input; j++) {
+			
+			// 0 und 1 auf true setzen, da sie sowieso keine Primzahlen sind
+			numbers[0] = true;
+			numbers[1] = true;
+		
+			
+			// Schleife um Primzahlen zu sortieren. 
+			for (int i = 2; i < input - i; i++) {
 				
-				if(j%i == 0&& j != i) {
-					numbers[j] = true;
+				//Schleife überprüft, ob Zahl sich um eine Primzahl handelt.
+				for (int j = i + 1; j < input; j++) {
+					
+					
+					if(j%i == 0) {
+						numbers[j] = true;
+					} 
+					 
+					
 				} 
-				 
-				
-			} 
-	
-		}
 		
-		System.out.print("Die Primzahlen bis " + (input-1) + " lauten: ");
-		
-		
-		// Ausgabe der Primzahlen
-		for (int k = 0; k < numbers.length; k++) {
-			
-			if (!numbers[k]) {
-				System.out.print(" [ "+k+" ] ");
 			}
 			
-		}	
+			System.out.print("Die Primzahlen bis " + (input-1) + " lauten: ");
+			
+			
+			// Ausgabe der Primzahlen
+			for (int k = 0; k < numbers.length; k++) {
+				
+				if (!numbers[k]) {
+					System.out.print(" [ "+k+" ] ");
+				}
+				
+			} 
+			
+		
+		
+		
+	
+		
+		
 
 	}
 
