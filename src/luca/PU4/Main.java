c45e816907e8d9911d4b486f89a5e616b3bfff92
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-05-18 12:41:36 +0200

    added a second case that user can choose to read from a file and edited the first case

--- src/luca/PU4/Main.java
+++ src/luca/PU4/Main.java
@@ -1,9 +1,10 @@
-package tut.pr1.hsm.de.a1.base;
+package luca.PU4;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
-import static tut.pr1.hsm.de.a1.base.Crypto.*;
+
+
 
 // Hauptklasse, die die Benutzeroberfläche für das Programm zur Verfügung stellt
 public class Main {
@@ -12,12 +13,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // Deklaration der Variablen für den verschlüsselten Text
         String  encryptedText;
+        String file;
         int shift;
 
         // Endlosschleife, um dem Benutzer wiederholt Optionen anzubieten
         while (true) {  // Benutzeroberfläche mit den verfügbaren Optionen
             System.out.println("Choose an operation:");
             System.out.println("1. Write a note");
+            System.out.println("2. Read a file");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
             int selection = scanner.nextInt(); // Liest die Benutzerauswahl ein
@@ -32,15 +35,28 @@ public class Main {
                     System.out.print("Enter shift value: ");
                     shift = scanner.nextInt();
                     scanner.nextLine(); // Entfernt den übriggebliebenen Zeilenumbruch nach der Zahleneingabe
+                    System.out.print("Enter filename: ");
+                    file = scanner.nextLine();
+                   
+                    
 
                     encryptedText = Crypto.caesarEncrypt(text, shift); // Verschlüsselt den eingegebenen Text
 
                     // Anpassung des Shift-Wertes, falls nötig, und Vorbereitung des verschlüsselten Texts für die Dateiausgabe
                     String encryptedTextWithShiftPrefix = Crypto.normalizeShiftValueAndSetShiftAsPrefix(shift, encryptedText);
 
-                    System.out.println("Encrypted text: " + encryptedTextWithShiftPrefix);
+                    System.out.println("Encrypted text written to file: " + encryptedTextWithShiftPrefix);
+                    FileHandler.writeToFile(file, encryptedTextWithShiftPrefix);
                     System.out.println();
                     break;
+                case 2: // Option 2: Lesen einer Notiz
+                	// Eingabeaufforderung für das die zulesenen Datei
+                	System.out.print("Enter filename: ");
+                	file = scanner.nextLine();
+                	text = FileHandler.readFromFile(file); // Sucht die eingebene Datei und übergibt den drin stehenden Text
+                	System.out.println(text);
+                	System.out.println();
+                	break;
 
                 case 0: // Option 0: Beenden des Programms
                     scanner.close(); // Schließen des Scanner-Objektes
