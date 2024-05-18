dcfe072443266fd91af7247113359c3349bde9b6
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-05-18 12:40:19 +0200

    added method that write a or to file and method that read a file

--- src/luca/PU4/FileHandler.java
+++ src/luca/PU4/FileHandler.java
@@ -1,6 +1,11 @@
-package tut.pr1.hsm.de.a1.base;
+package luca.PU4;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 // Klasse FileHandler zur Verwaltung der Dateioperationen
 public class FileHandler {
@@ -8,12 +13,28 @@ public class FileHandler {
 
     // Methode zum Schreiben eines Textes in eine Datei
     public static void writeToFile(String filename, String text) throws IOException {
-
+    	FileWriter ausgabestrom = new FileWriter(filename);
+		PrintWriter ausgabe = new PrintWriter(ausgabestrom);
+		ausgabe.println(text);
+		ausgabe.close();
+		
     }
 
     // Methode zum Lesen eines Textes aus einer Datei
     public static String readFromFile(String filename) throws IOException {
-
+    	String text = "";
+    	File file = new File(filename);
+		FileReader fr = new FileReader(file);
+		BufferedReader reader = new BufferedReader(fr);
+		
+		String line = reader.readLine();
+		while(line != null) {
+			text = text + line;
+			line = reader.readLine();
+		}
+		reader.close();
+    	
+    	return text;
     }
 }
 
